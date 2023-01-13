2f19cf901092047bb587f309b3fad60a17642491
Ivan <Ivan@DESKTOP-0ITQCBT>
2023-01-13 09:56:17 +0100

    Pruebas

---
+++ Ivan/pub/service.js
@@ -0,0 +1,17 @@
+app.service('myserv', function() {
+          this.getServiceList = function () {
+    return [];
+}
+this.getISEndpoint = function() { 
+ return 'http://DESKTOP-0ITQCBT:1111/';
+}
+this.getAPIList = function() { 
+ return [];
+}
+this.getCreatedTime = function() { 
+ return "13-01-2023 09:41:17 CET";
+}
+this.getPackageInfo = function(){
+ return{"packageName":"Ivan","createdDate":"13-01-2023 08:59:37 CET","version":"1.0"};
+}
+});
